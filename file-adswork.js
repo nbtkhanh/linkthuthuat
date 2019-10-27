0557bce823fcd63706fe6d3484f0964295beac47
nbtkhanh <32896029+nbtkhanh@users.noreply.github.com>
2019-10-27 09:02:00 +0700

    Add files via upload

---
+++ file-adswork.js
@@ -0,0 +1,3 @@
+function adtechltt(width,height) {
+document.write("<iframe id='adwork_bfg' name='ads' width='" + width + "' height='"+ height +"' frameborder='0' src='https://ads.adwork.bfgrelax.com/" + idayltt +"/" + idpub + ".html?w="+ width +"&h=" + height + "' marginwidth='0' marginheight='0' vspace='0' hspace='0' allowtransparency='true' scrolling='no' allowfullscreen='true'></iframe>")
+}
